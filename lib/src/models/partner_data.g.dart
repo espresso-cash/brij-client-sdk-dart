336f5acc342a4836465f1062c82a10b7eeaf98de
Justin Enerio <justinenerio@pm.me>
2025-02-26 01:47:54 +0800

    feat: added partner privacy and terms url (#22)

--- lib/src/models/partner_data.g.dart
+++ lib/src/models/partner_data.g.dart
@@ -10,10 +10,14 @@ _$PartnerModelImpl _$$PartnerModelImplFromJson(Map<String, dynamic> json) =>
     _$PartnerModelImpl(
       name: json['name'] as String,
       publicKey: json['publicKey'] as String,
+      privacyUrl: json['privacyUrl'] as String,
+      termsUrl: json['termsUrl'] as String,
     );
 
 Map<String, dynamic> _$$PartnerModelImplToJson(_$PartnerModelImpl instance) =>
     <String, dynamic>{
       'name': instance.name,
       'publicKey': instance.publicKey,
+      'privacyUrl': instance.privacyUrl,
+      'termsUrl': instance.termsUrl,
     };
