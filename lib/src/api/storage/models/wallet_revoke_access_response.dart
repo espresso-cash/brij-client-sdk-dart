624a693cab49b0a88f6b3999042d10b769161a33
Vlad Sumin <vladsum00@gmail.com>
2025-05-06 22:52:56 +0300

    upd

--- lib/src/api/storage/models/wallet_revoke_access_response.dart
+++ lib/src/api/storage/models/wallet_revoke_access_response.dart
@@ -2,8 +2,4 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint, unused_import
 
-<<<<<<<< HEAD:lib/src/api/storage/models/wallet_revoke_access_response.dart
 typedef WalletRevokeAccessResponse = dynamic;
-========
-typedef WalletRemoveUserDataResponse = dynamic;
->>>>>>>> main:lib/src/api/storage/models/wallet_remove_user_data_response.dart
